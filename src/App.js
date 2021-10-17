7595b734ef0fced36da97c45bd078fd0612d3c02
AswathyRajM <aswathyraj767@gmail.com>
2021-10-17 15:53:57 +0530

    add nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pizza</h1>
-    </div>
+    <Router>
+     <Navbar/>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Bars, Nav, NavIcon, NavLink } from './NavbarElements'
 
 export const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Nav>
+                <NavLink to='/'>Pizza</NavLink>
+                <NavIcon>
+                    <p>Menu</p>
+                    <Bars/>
+                </NavIcon>
+            </Nav>
+        </>
     )
 }
