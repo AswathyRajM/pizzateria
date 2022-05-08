0aa09fd281485ecfa06f4b8faa16530dcb865f2e
AswathyRajM <aswathyraj767@gmail.com>
2022-05-08 11:42:39 +0530

    manifest error fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
